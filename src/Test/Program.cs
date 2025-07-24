4c70c20d7471dc5569dadc0795f7c7a94ac0914b
vendatta-1 <81135582@fci.s-mu.edu.eg>
2025-07-24 19:40:50 +0300

    Convert From singleFile to csprog

---
+++ src/Test/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Test;
+
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            System.Console.WriteLine("No arguments provided.");
+            return;
+        }
+
+        string name = args[0];
+        System.Console.WriteLine($"Hello, {name}!");
+    }
+}
--- src/test.sh
+++ src/test.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
+ 
+CSHARP_FILE="src/Test/"
+ 
+ARG="Ashry"
 
-output=$(dotnet run Program.cs -- Ashry)
-expected="Hello, Ashry!"
+cd Test
 
-[[ "$output" == "$expected" ]] && echo "✅ PASS" || echo "❌ FAIL: $output"
+output=$(dotnet run -- "$ARG")
+ 
+expected="Hello, $ARG!"
+ 
+if [[ "$output" == "$expected" ]]; then
+    echo "✅ PASS"
+    exit 0
+else
+    echo "❌ FAIL"
+    echo "Expected: $expected"
+    echo "Got: $output"
+    exit 1
+fi 
